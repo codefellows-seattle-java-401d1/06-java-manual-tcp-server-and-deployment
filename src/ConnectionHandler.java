7a07eeca026212b47ac661b3efb96ad984bf31c4
Tara Johnson <tara@madface.com>
2018-07-18 12:31:18 -0700

    Update nickname in progress

--- src/ConnectionHandler.java
+++ src/ConnectionHandler.java
@@ -35,14 +35,46 @@ public class ConnectionHandler implements Runnable {
                 this.user.socket.close();
             } else if (line.startsWith("@list")) {
                 response = listUsers(line);
+            } else if (line.startsWith("@nickname")) {
+                response = setNickname(line);
             }
             // TODO: implement other command methods
+
 
             TCPServer.broadcast(this.user.toString() + ": " + response);
         }
     }
 
-    public String listUsers (String line) {
-        // TODO: implement list users
+    public static String listUsers(String line) {
+        String response = "";
+
+        for (User user : TCPServer.connections) {
+            response += user.toString() + "\n";
+        }
+
+        return response;
+    }
+
+    public String setNickname(String newNickname) throws IOException {
+        // Get current nickname
+        InputStream inputStream = this.user.socket.getInputStream();
+        InputStreamReader reader = new InputStreamReader(inputStream);
+        BufferedReader buffer = new BufferedReader(reader);
+
+        boolean isRunning = true;
+        while (isRunning) {
+            String line = buffer.readLine();
+            System.out.println(line);
+            isRunning = false;
+        }
+        // Assign current nickname to variable
+//        String originName = this.nickname.toString();
+
+        // User to change variable to new nickname
+
+
+        // Feedback to user that new nickname has been set
+        // Return nickname variable
+        return newNickname;
     }
 }
